c42faac1f021b176a9ec18df8ba480655b0938d5
rolandoamns <rolandoamns@gmail.com>
2021-03-28 12:24:15 -0500

    commit con models.py implementado

--- appcontrol/models.py
+++ appcontrol/models.py
@@ -1,3 +1,101 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+class persona (models.Model):
+    dni=models.CharField(max_length=8, primary_key=True, verbose_name="DNI")
+    apellido_paterno=models.CharField(max_length=200, verbose_name="Apellido paterno", blank=True)
+    apellido_materno=models.CharField(max_length=200,verbose_name="Apellido materno", blank=True)
+    nombres=models.CharField(max_length=200, verbose_name="Nombres") 
+    sexo=models.BooleanField()
+    fecha_nacimiento=models.DateField(verbose_name="Fecha de nacimiento", blank=True)
+    ubigeo_nacimiento=models.CharField(max_length=6, blank=True)
+    correo_institucional = models.CharField(max_length=200, blank=True,verbose_name="Correo institucional")
+    telefono = models.CharField(max_length=9, blank=True, verbose_name="Teléfono")
+    def __str__(self):
+        return  self.apellido_paterno + " " +self.apellido_materno+ " " +self.nombres
+
+
+class jefe(models.Model):
+     id_jefe = models.AutoField(primary_key=True, verbose_name="Id")
+     dni_jefe = models.ForeignKey(persona, on_delete=models.PROTECT, verbose_name="Jefe inmediato",related_name='rf_persona_jefe')
+     dni_empleado = models.ForeignKey(persona, on_delete=models.PROTECT, verbose_name="Subordinado",related_name='rf_persona_empleado')
+     estado_jefe = models.BooleanField(default=True, blank=True)
+     fechahora_creacion=models.DateField(default=datetime.datetime, blank=True)
+  
+     def __str__(self):
+        return self.id_jefe
+
+
+class periodo(models.Model):
+    id_periodo = models.AutoField(primary_key=True)
+    descripcion = models.CharField(max_length=100, verbose_name="Descripción")
+    detalle = models.CharField(max_length=100, verbose_name="Detalle")
+    dias_laborables = models.IntegerField(verbose_name="Total de días laborable")
+    observacion = models.CharField(max_length=100, verbose_name="Observación", blank=True)
+    estado_periodo = models.BooleanField(default=True, blank=True)
+
+    def __str__(self):
+        return self.descripcion
+
+
+
+class dependencia (models.Model):
+    id_dependencia = models.AutoField(primary_key=True, verbose_name="Id")
+    nombre_dependencia = models.CharField(max_length=200, verbose_name="Nombre")
+    abreviatura_dependencia = models.CharField(max_length=20, verbose_name="Abreviatura")
+    id_dep_tip = models.IntegerField(verbose_name="Id tipo")
+    id_sede = models.IntegerField(verbose_name="Id Sede")
+    id_depe_superior = models.IntegerField(verbose_name="Id Superior")
+    codigo_depe = models.CharField(max_length=8, verbose_name="Código")
+    estado_depe = models.BooleanField(default=True, blank=True)
+
+def __str__ (self):
+    return self.nombre_dependencia
+
+
+class tipoactividad (models.Model):
+    id_tipoactividad = models.AutoField(primary_key=True, verbose_name="Id")
+    nombre_tipoactividad = models.CharField(max_length=100, verbose_name="Nombre")
+    abreviatura_tipoactividad = models.CharField(max_length=20, verbose_name="Abreviatura")
+
+    def __str__(self):
+        return self.nombre_tipo
+
+
+class actividad(models.Model):
+    id_actividad = models.AutoField(primary_key=True, verbose_name="Id")
+    id_periodo =models.ForeignKey(periodo, on_delete=models.PROTECT)
+    id_tipoactividad = models.ForeignKey(tipoactividad, on_delete=models.PROTECT)
+    dni = models.ForeignKey(persona, on_delete=models.PROTECT)
+
+    telefono =models.CharField(max_length=9, verbose_name="Teléfono", blank=True)
+    correo = models.CharField(max_length=100, verbose_name="Correo institucional", blank=True)
+    
+    id_dependencia = models.ForeignKey(dependencia, on_delete=models.PROTECT),
+    nombre_dependencia = models.CharField(max_length=200, verbose_name="Dependencia", blank=True)
+    
+    gr_comorbilidad = models.BooleanField(verbose_name="Grupo de riesgo por comorbilidad", blank=True),
+    detalle_comorbilidad = models.CharField(max_length=200, verbose_name="Detalle de comorbilidad", blank=True)
+    
+    tiene_informe = models.BooleanField(default=True, blank=True, verbose_name="Tiene informe?")
+    actividad_resumen = models.CharField(max_length=200, verbose_name="Resumen de actividad", blank=True)
+    producto_entregado = models.CharField(max_length=200, verbose_name="Producto entregado", blank=True)
+    medio_verificacion = models.CharField(max_length=200, verbose_name="Medio de verificación", blank=True),
+    link_informe = models.CharField(max_length=200, verbose_name="Link de informe", blank=True),   
+    modalidad = models.CharField(max_length=200, verbose_name="Modalidad que trabajo", blank=True),
+    dias_laborado = models.IntegerField( verbose_name="Resumen de actividad", blank=True)
+
+    dni_jefe = models.ForeignKey(persona, on_delete=models.PROTECT),
+    nombre_jefe = models.CharField(max_length=100, verbose_name="Jefe inmediato", blank=True),
+    correo_jefe = models.CharField(max_length=100, verbose_name="Correo de jefe inmediato", blank=True),
+    telefono_jefe = models.CharField(max_length=9, verbose_name="Télefono", blank=True),
+    documento = models.CharField(max_length=200, verbose_name="Documento de conformidad", blank=True),
+    link_documento = models.CharField(max_length=200, verbose_name="Link de documento", blank=True),   
+    observacion  = models.CharField(max_length=200, verbose_name="Observación", blank=True),
+
+
+
+
+
+
--- pjcontrol/settings.py
+++ pjcontrol/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -93,6 +94,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'dbcontrol',
+        # 'USER': 'root',
+        # 'PASSWORD': 'Pemjord365.',
+        # 'HOST': 'localhost',
+        # 'PORT': 3306
     }
 }
 
